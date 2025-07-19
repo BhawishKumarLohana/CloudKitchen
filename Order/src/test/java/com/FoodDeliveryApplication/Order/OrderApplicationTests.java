7ef04a2d64d0c199174799d7d21c49a6492e3e06
Bhawish Kumar <23205172@life.hkbu.edu.hk>
2025-07-20 01:35:45 +0500

    orderMS

---
+++ Order/src/test/java/com/FoodDeliveryApplication/Order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodDeliveryApplication.Order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
